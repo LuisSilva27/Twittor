e68a7a173f28854391108b9744f1541c889730ff
= <=>
2021-01-26 11:13:22 -0500

    first commit

---
+++ js/sw-utils.js
@@ -0,0 +1,15 @@
+//Guardar el caché dinamico
+function actualizaCacheDinamico(dynamicCache,req,resp){
+    if(resp.ok ){
+        return caches.open(dynamicCache).then(cache=>{
+
+            cache.put(req,resp.clone());
+            return resp.clone();
+        });
+    }else{
+        return resp;
+    }
+
+
+
+}
