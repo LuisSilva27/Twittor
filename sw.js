1dde37d376a5e37add881087e2dff2cf2d0ec137
= <=>
2021-01-26 15:58:17 -0500

    modificaciones Ios

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
-const STATIC_CACHE= 'static-v1.1';
+const STATIC_CACHE= 'static-v2';
 const INMUTABLE_CACHE= 'inmutable-v1';
-const DYNAMIC_CACHE= 'dynamic-v1';
+const DYNAMIC_CACHE= 'dynamic-v2';
 
 const APP_SHELL=[
     // '/',
